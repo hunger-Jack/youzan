4186a19af0cef067e36096db3f4d5854eda7a514
jack <2959921118@qq.com>
2018-02-10 04:27:57 +0800

    goods-addcart

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -6,7 +6,8 @@ let url = {
     rank: '/category/rank',
     searchList: '/search/list',
     details: '/goods/details',
-    deal: '/goods/deal'
+    deal: '/goods/deal',
+    addCart: '/cart/add'
 }
 
 let host = 'http://rapapi.org/mockjsdata/24170'
--- src/pages/goods/goods.js
+++ src/pages/goods/goods.js
@@ -4,6 +4,7 @@ import './goods.css'
 import './goods_theme.css'
 import './goods_mars.css'
 import './goods_sku.css'
+import './goods_transition.css'
 import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
@@ -22,12 +23,18 @@ new Vue({
   el: '#app',
   data() {
     return {
+      id,
       details: null,
       changeTabs,
       curIndex: 0,
       dealLists: null,
       loading: false,
-      bannerLists: []
+      bannerLists: [],
+      skuType: 0,//三种状态
+      isShowSku: false,//是否显示遮罩层和规格选择弹框
+      skuNum: 1,//购买数量
+      isAddCart: false,//是否成功添加购物车
+      isAddCartMessage: false//添加购物车成功弹窗信息
     }
   },
   methods: {
@@ -59,10 +66,36 @@ new Vue({
         this.loading = false
         this.dealLists = res.data.data.lists
       })
+    },
+    chooseSku(type) {//绑定规格选择，加入购物车，立即购买三种状态
+      this.skuType = type
+      this.isShowSku = true
+    },
+    chooseSkuNum(num) {//数量plus和minus，如果减少到数量为1，直接返回
+      if(num<0 && this.skuNum === 1) return
+      this.skuNum += num
+    },
+    addCart() {
+      axios.post(url.addCart,{id,number:this.skuNum}).then((res) => {
+        if(res.data.status === 200) {
+          this.isShowSku = false
+          this.isAddCart = true
+          this.isAddCartMessage = true
+          setTimeout(()=>{this.isAddCartMessage=false},1000)//加入购物车成功弹窗吗，给个定时器改变状态
+        }
+      })
     }
   },
   created() {
     this.getDetails()
+  },
+  watch: {
+    isShowSku(newVal,oldVal) {//监听isShowSku,如果弹出阴影层，固定body和html宽度，如果没有就auto
+      document.body.style.overflow = (newVal ? 'hidden' : 'auto')
+      document.querySelector('html').style.overflow = (newVal ? 'hidden' : 'auto')
+      // document.body.style.height = (newVal ? '100%' : 'auto')
+      // document.querySelector('html').style.height = (newVal ? '100%' : 'auto')
+    }
   },
   components: {
     Swipe
